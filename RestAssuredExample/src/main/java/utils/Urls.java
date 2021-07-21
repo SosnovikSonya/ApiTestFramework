170efdfa2d5e1860e0ea0157f9b57dcdd6401287
SosnovikSonya <sonya_sosnovik2001@mail.ru>
2021-07-21 12:03:55 +0300

    add some helper classes

--- RestAssuredExample/src/main/java/utils/Urls.java
+++ RestAssuredExample/src/main/java/utils/Urls.java
@@ -1,10 +1,9 @@
 package utils;
 
-public class Routes {
+public class Urls {
     public final static String baseUrl = "http://users.bugred.ru/tasks/rest";
     public final static String getUserUrl = "/getuser";
     public final static String createUserUrl = "/doregister";
     public final static String deleteUserUrl = "/deleteuser";
     public final static String updateUserFieldUrl = "/useronefield";
-    public final static String addAvatarUrl = "/addavatar";
 }
